850ae9960a9584f8041b65e14734855904ce5fcb
Daniel Brice <danielbrice@gmail.com>
2018-06-08 12:01:39 -0700

    this is one way to do it

--- src/main/scala/localhost/sangriademo/Main.scala
+++ src/main/scala/localhost/sangriademo/Main.scala
@@ -1,7 +1,6 @@
 package localhost.sangriademo
 
 import argonaut.Json
-import localhost.sangriademo.guts.Eithers._
 import localhost.sangriademo.guts.FalsoDB
 import sangria.ast.Document
 import sangria.execution.{Executor, ValidationError}
@@ -10,8 +9,8 @@ import sangria.parser.{QueryParser, SyntaxError}
 
 import scala.io.Source
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.util.Try
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 object Main extends App {
 
@@ -20,63 +19,63 @@ object Main extends App {
   def format(msg: String): String =
     s"""{"data":null,"errors":[{"message":"${msg.filterNot(_.isControl)}"}]}"""
 
+  case class Err(response: Response) extends Exception(response._3)
+
   def handlePost(postBody: String): Response = {
+    try {
 
-    // An `Either[L, R]` is `Left(x: L)` or `Right(y: R)`.
-    val maybeQuery: Either[Response, String] = {
-
-      // An `Option[A]` is `None()` or `Some(x: A)`.
-      val foo: Option[String] =
-        argonaut.Parse.parseOption(postBody)
-          .flatMap(json => json.field("query"))
-          .flatMap(queryField => queryField.string)
-
-      foo.ifFailed {
-        val msg = s"Unable to parse request body: $postBody"
-        (401, "application/json", format(msg))
+      // Grab the GraphQL query out of the request body.
+      val query: String = {
+        try {
+          argonaut.Parse.parseOption(postBody)
+            .flatMap(json => json.field("query"))
+            .flatMap(queryField => queryField.string)
+            .get
+        }
+        catch {
+          case _: Throwable =>
+            val msg = s"Unable to parse request body: $postBody"
+            throw Err( (401, "application/json", format(msg)) )
+        }
       }
-    }
 
-    val maybeParsedQuery: Either[Response, Document] =
-      maybeQuery.flatMap { query =>
-
-        // A `Try[A]` is `Success(x: A)` or `Failure(e: Throwable)`.
-        val foo: Try[Document] =
-          QueryParser.parse(query) // From Sangria. Parses a GraphQL query.
-
-        foo.ifFailed {
+      // Make sure the GraphQL query is syntactically valid.
+      val parsedQuery: Document = {
+        try {
+          QueryParser.parse(query).get // From Sangria. Parses query.
+        }
+        catch {
           case err: SyntaxError =>
-            (401, "application/json", format(err.getMessage))
-          case err =>
-            (500, "application/json", format(err.getMessage))
+            throw Err( (401, "application/json", format(err.getMessage)) )
+          case err: Throwable =>
+            throw Err( (500, "application/json", format(err.getMessage)) )
         }
       }
 
-    val maybeExecutedQuery: Either[Response, Json] =
-      maybeParsedQuery.flatMap { parsedQuery =>
-
-        // A `Future[A]` is `Pending()`, `Done(x: A)` or `Failed(e: Throwable)`
-        val foo: Future[Json] =
-          Executor.execute( // From Sangria.
-            queryAst = parsedQuery,          // Accepts the query.
-            schema = SchemaDef.schema,       // Validates against the Schema.
-            userContext = FalsoDB.appContext // Executes using our AppContext.
-          )
-
-        foo.ifFailed {
+      // Make sure the GraphQL query fits our schema and execute it.
+      val executedQuery: Json = {
+        try {
+          val futureJson: Future[Json] =
+            Executor.execute( // From Sangria.
+              queryAst    = parsedQuery,       // Accepts the query.
+              schema      = SchemaDef.schema,  // Validates against the Schema.
+              userContext = FalsoDB.appContext // Executes using our AppContext.
+            )
+          Await.result(futureJson, Duration.Inf)
+        }
+        catch {
           case err: ValidationError =>
-            (401, "application/json", err.resolveError.nospaces)
-          case err =>
-            (500, "application/json", format(err.getMessage))
+            throw Err( (401, "application/json", err.resolveError.toString) )
+          case err: Throwable =>
+            throw Err( (500, "application/json", format(err.getMessage)) )
         }
       }
 
-    val finalResponse: Either[Response, Response] =
-      maybeExecutedQuery.map { json =>
-        (200, "application/json", json.nospaces)
-      }
-
-    finalResponse.converge
+      (200, "application/json", executedQuery.toString)
+    }
+    catch {
+      case Err(response) => response
+    }
   }
 
   serve(8080) {
